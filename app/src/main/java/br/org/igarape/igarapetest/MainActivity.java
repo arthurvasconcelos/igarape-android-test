082ba065d21f2f6bfad7031f933bb177e47df34e
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2015-11-13 02:51:24 -0200

    Solution to show lat, long and address in screen

--- app/src/main/java/br/org/igarape/igarapetest/MainActivity.java
+++ app/src/main/java/br/org/igarape/igarapetest/MainActivity.java
@@ -1,16 +1,77 @@
 package br.org.igarape.igarapetest;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView lat;
+    TextView lon;
+    TextView geo;
+    String bestProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        bestProvider = locationManager.getBestProvider(criteria, false);
+
+        lat = (TextView) findViewById(R.id.lat);
+        lon = (TextView) findViewById(R.id.lon);
+        geo = (TextView) findViewById(R.id.geo);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(final Location location) {
+                lat.setText(String.format("%.6f", location.getLatitude()));
+                lon.setText(String.format("%.6f", location.getLongitude()));
+                Geocoder geocoder = new Geocoder(getApplicationContext());
+                try {
+                    List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    if (addressList.size() == 1)
+                        geo.setText(addressList.get(0).getAddressLine(0));
+                    else
+                        geo.setText("unknown");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+            @Override
+            public void onProviderEnabled(String provider) {}
+            @Override
+            public void onProviderDisabled(String provider) {}
+        };
+
+        locationManager.requestLocationUpdates(bestProvider, 400, 1, locationListener);
+    }
+
+    public void getMe(View v) {
+        lat.setText("-22.9558282");
+        lon.setText("-43.1973136");
+        geo.setText("Instituto Igarapé - R. Conde de Irajá, 370 - Botafogo");
+
+        Toast.makeText(getBaseContext(), "Coolest Place!!! =D", Toast.LENGTH_LONG).show();
     }
 
     @Override
